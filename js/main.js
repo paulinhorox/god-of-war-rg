072dbc2a0ff2b2d52821d1581bad20e60ca4bad9
Paulo Eduardo <81439306+paulinhorox@users.noreply.github.com>
2022-12-06 08:01:53 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".slide-characters", {
+    slidesPerView: 3.5,
+    spaceBetween: 19,
+    freeMode: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1.2,
+        spaceBetween: 19
+      },
+
+
+      768: {
+        slidesPerView: 2.2,
+        spaceBetween: 19
+      },
+
+      991: {
+        slidesPerView: 2.8,
+      },
+
+      1200: {
+        slidesPerView: 3.5
+      }
+    }
+
+  });
+AOS.init({
+  duration: 1000,
+  once: true,
+})
